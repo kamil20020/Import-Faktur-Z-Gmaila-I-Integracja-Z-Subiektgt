06694c5acc21ad082352c658a043bd66d1ed84d1
kdywan_254049 <idywan_254049@e-science.pl>
2025-06-26 01:38:11 +0200

    Fixed exe

--- kod/sfera-nakladka/start.bat
+++ kod/sfera-nakladka/start.bat
@@ -5,7 +5,7 @@ cd /d "%~dp0"
 start "" /b php\php.exe -c php\php.ini-production -S 127.0.0.1:80 -t htdocs
 set PHP_PID=%!
 
-start "" /b javaw -jar aplikacja-gmail_subiekt-integracja.jar
+start "" /b java -jar aplikacja-gmail_subiekt-integracja.jar
 set JAVA_PID=%!
 
 pause
